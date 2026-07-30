74d16493ce70ac0bd72a3950f39dbc0e27f61750
kiranpate1 <kp8568@gmail.com>
2026-07-29 23:59:34 -0400

    added dyn island

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <div className="relative w-[393px] h-[852px] bg-white rounded-[120px]"></div>
+    <main className="w-dvw h-dvh flex flex-col items-center justify-center overflow-hidden">
+      <div className="relative w-[393px] h-[852px] bg-white">
+        <Image
+          className="absolute inset-[0_0_auto_0]"
+          src="/dynamic-island.png"
+          alt="Logo"
+          width={600}
+          height={600}
+        />
+      </div>
     </main>
   );
 }
